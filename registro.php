db7ff48a251c3a6c5b4426b7a8f8016cca0b9de1
Maxi-Max10 <88356143+Maxi-Max10@users.noreply.github.com>
2022-05-14 03:17:58 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -8,8 +8,24 @@ $localidad=$_POST['localidad'];
 $provincia=$_POST['provincia'];
 $codP=$_POST['codP'];
 $sexo=$_POST['sexo'];
-$estudios=$_POST['estudios'];
+$estudios = '';
+$estudios= implode( ', ', $_POST['estudios']); //junto todos los estudios 
+
 $cursos=$_POST['cursos'];
+
+
+$user = "root";
+$pass = "root";
+$host = "localhost:3308";
+$db =  "formulario";
+
+
+$con = mysqli_connect($host, $user, $pass, $db);
+
+
+$sql = "INSERT INTO datos VALUES(null, '$dni', '$nombre', '$apellido', '$domicilio', '$localidad', '$provincia', '$codP', '$sexo', '$estudios', '$cursos')";
+$resultado = mysqli_query($con, $sql);
+
 
 
 echo "EL DNI INGRESADO ES: $dni <br/>"; 
